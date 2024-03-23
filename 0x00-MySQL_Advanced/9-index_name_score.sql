eda9ad2f553ded5ee6799c77a87e13e84d956fca
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-23 06:47:12 +0000

    Optimize search and score

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,4 @@
+-- 9-index_name_score.sql
+-- Task: Create an index idx_name_first_score on the table names and the first letter of name and the score
+
+CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
