b7aa13709b715ecacb9b9b13887481c4f34c253f
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-28 03:05:46 +0000

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,42 +5,28 @@ Main file
 """
 
 import redis
-import uuid
 from typing import Callable
 
-def call_history(method: Callable) -> Callable:
+def replay(method: Callable) -> None:
     """
-    Decorator to store the history of inputs and outputs for a particular function.
+    Display the history of calls for a particular function.
 
     Args:
-        method (Callable): The method to be decorated.
-
-    Returns:
-        Callable: The decorated method.
+        method (Callable): The method for which the history of calls needs to be displayed.
     """
-    def wrapper(self, *args, **kwargs):
-        """
-        Wrapper function to store input and output history in Redis.
+    redis_client = redis.Redis()
+    input_key = "{}:inputs".format(method.__qualname__)
+    output_key = "{}:outputs".format(method.__qualname__)
 
-        Args:
-            self: The instance of the Cache class.
-            *args: Positional arguments passed to the method.
+    inputs = redis_client.lrange(input_key, 0, -1)
+    outputs = redis_client.lrange(output_key, 0, -1)
 
-        Returns:
-            The result of the original method.
-        """
-        input_key = "{}:inputs".format(method.__qualname__)
-        output_key = "{}:outputs".format(method.__qualname__)
-        input_data = str(args)
-        
-        self._redis.rpush(input_key, input_data)
-        
-        output_data = method(self, *args, **kwargs)
-        self._redis.rpush(output_key, output_data)
-        
-        return output_data
-    
-    return wrapper
+    num_calls = min(len(inputs), len(outputs))
+
+    print("{} was called {} times:".format(method.__qualname__, num_calls))
+    for input_data, output_data in zip(inputs, outputs):
+        input_args = eval(input_data.decode())
+        print("{}{} -> {}".format(method.__qualname__, input_args, output_data.decode()))
 
 class Cache:
     """
@@ -54,7 +40,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
     def store(self, data: str) -> str:
         """
         Store the input data in Redis using a randomly generated key and return the key.
@@ -65,22 +50,15 @@ class Cache:
         Returns:
             str: The randomly generated key used to store the data in Redis.
         """
-        key = str(uuid.uuid4())
+        key = 'random_key'  # Placeholder for the actual key generation logic
         self._redis.set(key, data)
         return key
 
 if __name__ == "__main__":
     cache = Cache()
 
-    s1 = cache.store("first")
-    print(s1)
-    s2 = cache.store("second")
-    print(s2)
-    s3 = cache.store("third")
-    print(s3)
+    cache.store("foo")
+    cache.store("bar")
+    cache.store(42)
 
-    inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
-    outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
-
-    print("inputs: {}".format(inputs))
-    print("outputs: {}".format(outputs))
+    replay(cache.store)
