7ac7370750208d45371346aecd6b19a805cfbb4e
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-28 02:52:58 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ Main file
 
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union
 
 class Cache:
     """
@@ -34,12 +34,57 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis for the given key. Optionally, apply a conversion function.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+            fn (Callable, optional): A callable function to convert the retrieved data.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data from Redis.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieve data from Redis for the given key and convert it to a UTF-8 encoded string.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            Union[str, None]: The retrieved data as a UTF-8 encoded string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieve data from Redis for the given key and convert it to an integer.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            Union[int, None]: The retrieved data as an integer.
+        """
+        return self.get(key, fn=int)
+
 if __name__ == "__main__":
     cache = Cache()
 
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
 
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
